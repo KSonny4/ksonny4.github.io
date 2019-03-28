4a08c813761c3faa708c457c446047948ef0c90e
ksonny4 <ksonny4@gmail.com>
2019-03-28 22:03:51 +0100

    script for no crash download edited

--- files/download_even_if_interrupted.sh
+++ files/download_even_if_interrupted.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#I have to save this script because
-#I have unstable internet connection and wget provides me 
+#I have created this script because
+#I have unstable internet connection and wget provides me
 # a way how to download the file in contrast with browser
 # that simply says that the file is corrupted
-while ! wget -c $0 do sleep 1; done
+while ! wget -c $1; do sleep 1; done
